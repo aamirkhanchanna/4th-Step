c6551c9428bd18ba0abac59f42fbdc04d1c4ab52
Aamir Khan Channa <160345903+aamirkhanchanna@users.noreply.github.com>
2024-03-12 16:48:45 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,83 @@
+// //Logical Opperator
+// // 1 (& Number)   2no True
+// 1st Way
+// let num1 = 10;
+// let num2 = 5;
+// console.log(num1 == num2);  //False
+
+
+// let num1 = 10;
+// let num2 = 5;
+// console.log(num1 == 10 && num2 == 5);
+//           10  == 10   5  == 5 True
+
+// // console.log(num1 == 5 && num2 == 10);
+//                 10  == 5     10  == 5 False
+
+
+
+// // 2 (R Number) 1 True
+// let num1 = 10;
+// let num2 = 5;
+// console.log(num1 == 10 || num2 == num1);  // True
+
+
+
+// // 3 (Not Opperator) Opposite Working
+// // 1st Way
+// let num1 = 10;
+// let num2 = 5;
+// console.log(num1 == num2);
+// //           10  ==  5   False
+
+// // 2nd Way
+// let num1 = 10;
+// let num2 = 5;
+// console.log(!(num1 == num2));
+//             10  ==  5  True
+
+
+
+// // 1 (if and if-else)
+
+
+// let num1 = 10
+// if(num1 == 10){
+// //   10 == 10 
+//     console.log("Hello Iam Aamir Khan")
+// }else{
+//     console.log("under else")
+// }
+
+
+// // 2 Else If Statement
+// // 1st Way
+// let num1 = 10
+// if(num1 == 6){
+//     console.log("number is 6")
+
+// }else{
+//     console.log(" number is above ")
+// }
+
+// // 2nd Way
+// let num1 = 10
+// if(num1 == 6){
+//     console.log("number is 6")
+
+// }else if(num1 == 8)
+// {
+//     console.log("number is not 8")
+// }
+// else if (num1 == 10){
+//     console.log("number is right")
+// }
+
+
+// // 3rd Way String Type
+// let name1:string = "Aamir";
+// if(name1 == "Aamir"){
+//     console.log("name is Aamir")
+// }else{
+//     console.log("name is not Aamir");
+// }
